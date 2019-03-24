2578a2f03af7b01c51655865a4c4e07c5fcc3ddb
Loris Rossi <lorisrossirn@gmail.com>
2019-03-24 18:10:50 +0100

    Add Service resource to sample-app.deploy

--- prometheus/hack/prometheus-down.sh
+++ prometheus/hack/prometheus-down.sh
@@ -1,9 +1,15 @@
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 kubectl delete -f ${SCRIPT_ROOT}/deploy/prom-operator.yaml
-kubectl delete -f ${SCRIPT_ROOT}/deploy/deployment-example-app.yaml
-kubectl delete -f ${SCRIPT_ROOT}/deploy/service-example-app.yaml
-kubectl delete -f ${SCRIPT_ROOT}/deploy/servicemonitor-example-app.yaml
+
+# kubectl delete -f ${SCRIPT_ROOT}/deploy/deployment-example-app.yaml
+# kubectl delete -f ${SCRIPT_ROOT}/deploy/service-example-app.yaml
+# kubectl delete -f ${SCRIPT_ROOT}/deploy/servicemonitor-example-app.yaml
+kubectl delete -f ${SCRIPT_ROOT}/deploy/sample-app.deploy.yaml
+kubectl delete -f ${SCRIPT_ROOT}/deploy/servicemonitor-sample-app.yaml
+
 kubectl delete -f ${SCRIPT_ROOT}/deploy/rbac-prometheus-pods.yaml
 kubectl delete -f ${SCRIPT_ROOT}/deploy/prometheus.yaml
 kubectl delete -f ${SCRIPT_ROOT}/deploy/service-prometheus.yaml
+
+# kubectl delete namespace prom
