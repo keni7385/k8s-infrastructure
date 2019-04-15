2eed53b62633cc0a736e3c500df32436c7a22a35
Loris Rossi <lorisrossirn@gmail.com>
2019-04-15 15:40:41 +0200

    Clean up old examples to test prometheus

--- prometheus/hack/prometheus-down.sh
+++ prometheus/hack/prometheus-down.sh
@@ -1,13 +1,6 @@
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 kubectl delete -f ${SCRIPT_ROOT}/deploy/prom-operator.yaml
-
-# kubectl delete -f ${SCRIPT_ROOT}/deploy/deployment-example-app.yaml
-# kubectl delete -f ${SCRIPT_ROOT}/deploy/service-example-app.yaml
-# kubectl delete -f ${SCRIPT_ROOT}/deploy/servicemonitor-example-app.yaml
-# kubectl delete -f ${SCRIPT_ROOT}/deploy/sample-app.deploy.yaml
-# kubectl delete -f ${SCRIPT_ROOT}/deploy/servicemonitor-sample-app.yaml
-
 kubectl delete -f ${SCRIPT_ROOT}/deploy/rbac-prometheus-pods.yaml
 kubectl delete -f ${SCRIPT_ROOT}/monitoring-nginx/prometheus-nginx-ingress.yaml
 kubectl delete -f ${SCRIPT_ROOT}/monitoring-nginx/servicemonitor-nginx-ingress.yaml
--- prometheus/hack/prometheus-up.sh
+++ prometheus/hack/prometheus-up.sh
@@ -1,15 +1,7 @@
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 
 kubectl create -f ${SCRIPT_ROOT}/deploy/prom-operator.yaml
-
-# kubectl create -f ${SCRIPT_ROOT}/deploy/deployment-example-app.yaml
-# kubectl create -f ${SCRIPT_ROOT}/deploy/service-example-app.yaml
-# kubectl create -f ${SCRIPT_ROOT}/deploy/servicemonitor-example-app.yaml
-# kubectl create -f ${SCRIPT_ROOT}/deploy/sample-app.deploy.yaml
-# kubectl create -f ${SCRIPT_ROOT}/deploy/servicemonitor-sample-app.yaml
-
 kubectl create -f ${SCRIPT_ROOT}/deploy/rbac-prometheus-pods.yaml
 kubectl create -f ${SCRIPT_ROOT}/monitoring-nginx/prometheus-nginx-ingress.yaml
 kubectl create -f ${SCRIPT_ROOT}/monitoring-nginx/servicemonitor-nginx-ingress.yaml
 kubectl create -f ${SCRIPT_ROOT}/deploy/service-prometheus-nginx.yaml
-
